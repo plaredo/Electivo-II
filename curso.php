2cc6cf20b21dfa3ff5355c1e61f2ef90df73461a
LabCom <LabCom@DESKTOP-R4U7GJA>
2016-10-05 15:15:30 -0500

    curso de electivo

--- curso.php
+++ curso.php
@@ -5,4 +5,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+//clase de curso
+class curso {
+    var $codigo;
+    var $nombre;
+    var $hora;
+    var $credito;
+
+}
 
